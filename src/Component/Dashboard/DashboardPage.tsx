5364953ac51f0913c9d23e38de8b63be96cab194
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-12-03 02:23:08 +0600

    feat(dashboardPage): dashboardPage update

--- src/Component/Dashboard/DashboardPage.tsx
+++ src/Component/Dashboard/DashboardPage.tsx
@@ -1,8 +1,8 @@
 // DashboardPage.tsx
-import { Outlet } from "react-router-dom";
-import AppSidebar from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import Navbar from "../DashboardNavbar/DahsboardNavbar";
+import { Outlet } from 'react-router-dom';
+import AppSidebar from '@/components/app-sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import DashboardNavbar from '../DashboardNavbar/DahsboardNavbar';
 
 const DashboardPage = () => {
   return (
@@ -21,7 +21,7 @@ const DashboardPage = () => {
         {/* Main content */}
         <main className="flex-1 flex flex-col min-h-screen overflow-auto p-4">
           {/* Navbar */}
-          <Navbar />
+          <DashboardNavbar />
 
           {/* Page content */}
           <div className="flex-1">
