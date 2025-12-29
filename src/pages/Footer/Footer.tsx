8dbfdfa3eb196fa03aafbf34db340d682ffd3253
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-12-29 06:11:27 +0600

    Footer: Update the footer page

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,6 +1,9 @@
 // Footer.jsx
-import React from "react";
+
 import Swal from "sweetalert2";
+import mapImg from "../../assets/map.svg"
+import callImg from "../../assets/calll.svg"
+import msgSvg from "../../assets/msg.svg"
 
 const Footer = () => {
   const logoSrc =
@@ -145,7 +148,7 @@ const Footer = () => {
               <li><a href="/" className="hover:text-teal-400 transition-colors duration-200">Home</a></li>
               <li><a href="/about" className="hover:text-teal-400 transition-colors duration-200">About Us</a></li>
               <li><a href="/list-with-us" className="hover:text-teal-400 transition-colors duration-200">List With Us</a></li>
-              <li><a href="/contact" className="hover:text-teal-400 transition-colors duration-200">Contact</a></li>
+              <li><a href="/contact" className="hover:text-teal-400 transition-colors duration-200 ">Contact</a></li>
               <li>
            
                 <a
@@ -163,9 +166,7 @@ const Footer = () => {
           <div className="text-center sm:text-left md:text-left">
             <h4 className="text-lg md:text-xl font-semibold mb-4 md:mb-6">Address</h4>
             <div className="flex items-start justify-center sm:justify-start md:justify-start space-x-3">
-              <svg className="h-6 w-6 text-teal-400 flex-shrink-0 mt-1" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-              </svg>
+           <img src={mapImg} alt=""  className="lg:mt-4"/>
               <p className="text-sm md:text-base">
                 #65 Husbands Gardens,<br />St. James, Barbados BB 23042
               </p>
@@ -174,20 +175,15 @@ const Footer = () => {
 
           {/* Contact */}
           <div className="text-center sm:text-left md:text-left -mt-5">
-            <h4 className="text-lg md:text-xl font-semibold mb-4 md:mb-6">Contact</h4>
+            <h4 className="text-lg md:text-xl font-semibold mb-4 md:mb-6  lg:mt-4">Contact</h4>
             <div className="space-y-2 md:space-y-3">
               <div className="flex items-center justify-center sm:justify-start md:justify-start space-x-3">
-                <svg className="h-6 w-6 text-teal-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06L6.222 10.27A10 10 0 0011.72 13.78l1.624-1.624a1 1 0 011.06-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.163 18 2 12.837 2 6V3z" />
-                </svg>
+                <img src={callImg} alt="" />
                 <p className="text-sm md:text-base">+1 (246) 233-EAST</p>
               </div>
 
               <div className="flex items-center justify-center sm:justify-start md:justify-start space-x-3">
-                <svg className="h-6 w-6 text-teal-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0018 4H2a2 2 0 00-.003 1.884z" />
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                </svg>
+                <img src={msgSvg} alt="" />
                 <p className="text-sm md:text-base">info@eastmondvillas.com</p>
               </div>
             </div>
