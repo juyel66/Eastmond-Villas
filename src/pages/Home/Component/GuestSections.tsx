194998ce25da099b37402955f8af3d91d6ac0adf
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-01-29 04:31:15 +0600

    feat(GuestSections): GuestSections slider added and api integrations

--- src/pages/Home/Component/GuestSections.tsx
+++ src/pages/Home/Component/GuestSections.tsx
@@ -5,7 +5,7 @@ const GuestSections = () => {
     return (
         <div>
             <div className="text-center items-center mt-16  ">
-                <h1 className="text-4xl font-semibold  mb-5">Guest Perspectives on Paradise Perfected</h1>
+                <h1 className="text-4xl font-semibold  mb-5">Guest Perspectives On Paradise Perfected</h1>
             <p>Whispers of Elegance from Those Who've Experienced the Extraordinary.</p>
             </div>
 
